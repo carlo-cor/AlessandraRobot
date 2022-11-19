ce757da15fd9fc0c023d59f55e46f8dd67c37b07
carlo-cor <carlo.cortez2022@mauihigh.org>
2022-11-18 16:58:32 -1000

    initial commit

---
+++ src/main/java/frc/robot/Drive.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+public class Drive {
+    private MotorControllerGroup left;
+    private MotorControllerGroup right;
+    private DifferentialDrive diffDrive;
+
+    public Drive(){
+        
+    }
+
+   //create constructor instantiating differential drive, & 2 groups: left and right side 
+   //pass 4 motors into Drive constructor 
+   //create arcade and tank drive methods 
+}
