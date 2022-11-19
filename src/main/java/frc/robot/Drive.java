0c4f3dfaac9093f931e7783cdd224afebfa269d4
carlo-cor <carlo.cortez2022@mauihigh.org>
2022-11-18 17:04:17 -1000

    happy birthday carlo - from alessandra, jovi, and edrich. pls come back and teach us github (finished drive class)

--- src/main/java/frc/robot/Drive.java
+++ src/main/java/frc/robot/Drive.java
@@ -1,19 +1,39 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.motorcontrol.MotorController; 
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class Drive {
-    private MotorControllerGroup left;
-    private MotorControllerGroup right;
-    private DifferentialDrive diffDrive;
 
-    public Drive(){
-        
-    }
+private MotorControllerGroup left; 
+private MotorControllerGroup right; 
+private DifferentialDrive diffDrive; 
 
-   //create constructor instantiating differential drive, & 2 groups: left and right side 
-   //pass 4 motors into Drive constructor 
-   //create arcade and tank drive methods 
+public Drive(MotorController fl, MotorController bl, MotorController fr, MotorController br){
+    left = new MotorControllerGroup(fl, bl);
+    right = new MotorControllerGroup(fr, br);
+    diffDrive = new DifferentialDrive(left, right);
+}
+
+private double deadzone(double num){
+    return Math.abs(num) > 0.1 ? num : 0;
+}
+
+public void arcade(double xSpeed, double zRotation){
+    diffDrive.arcadeDrive(xSpeed, zRotation);
+}
+
+public void arcadeRun(double xSpeed, double zRotation){
+    diffDrive.arcadeDrive(deadzone(xSpeed), deadzone(zRotation));
+}
+
+public void tank(double lSpeed, double rSpeed){
+    diffDrive.tankDrive(lSpeed, rSpeed);
+}
+
+public void tankRun(double lSpeed, double rSpeed){
+    diffDrive.tankDrive(deadzone(lSpeed), deadzone(rSpeed));
+}
+
 }
